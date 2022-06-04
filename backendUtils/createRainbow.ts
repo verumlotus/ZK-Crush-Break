680d7c4b25ac21a3f7754fcf5b5a2c27b298b6f6
verumlotus <universumtremendum@gmail.com>
2022-06-04 15:28:02 -0400

    scrapeLast Name

--- backendUtils/createRainbow.ts
+++ backendUtils/createRainbow.ts
@@ -20,4 +20,27 @@ async function scrapeFirstNames() {
     fs.writeFileSync(FILE_PATH, JSON.stringify(dataToWrite))
 }
 
-scrapeFirstNames();
+async function scrapeLastNames() {
+    const URL = "https://www2.census.gov/topics/genealogy/1990surnames/dist.all.last"
+    const response = await axios.get(URL)
+    // We need to retrieve just the last Names
+    // First, break each line up by white space (first elem will be the last name)
+    let lastNames = []
+    const splitByLine = response.data.split(/\r?\n/)
+    for (const line of splitByLine) {
+        const lineData = line.split(/\s+/)
+        lastNames.push(lineData[0])
+    }
+    // Delete our lastNames.json File if it exists
+    const FILE_PATH = path.resolve(__dirname, "lastNames.json");
+    if (fs.existsSync(FILE_PATH)) {
+        fs.unlinkSync(FILE_PATH)
+    }
+    // Write to our file now 
+    const dataToWrite = {
+        "lastNames": lastNames
+    }
+    fs.writeFileSync(FILE_PATH, JSON.stringify(dataToWrite))  
+}
+
+scrapeLastNames()
