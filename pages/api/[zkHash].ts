617bc67faa281826e0d4f36a2eb8b526d6300684
verumlotus <universumtremendum@gmail.com>
2022-06-04 19:00:57 -0400

    Ship it

--- pages/api/[zkHash].ts
+++ pages/api/[zkHash].ts
@@ -1,25 +1,17 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 import prisma from "../../backendUtils/dbAccess"
-import { useRouter } from "next/router";
-import { URLSearchParams } from 'url';
 
 type Data = {
   name: string
 }
 
-export default async function rainbowName(req: NextApiRequest, res: NextApiResponse<Data>) {
-  const body = JSON.parse(req.body)
-  if (!("zkUrl" in body)) {
-    return res.send({"name": "ZK Url not submitted to API!"})
-  }
-
-  const urlParams = new URLSearchParams(body["zkUrl"])
-  const zkHash = urlParams.get('hash')
+export default async function hashToMap(req: NextApiRequest, res: NextApiResponse<Data>) {
+  const {zkHash = ""} = req.query
 
   let dbData = await prisma.rainbowTable.findFirst({
     where: {
-      hash: zkHash
+      hash: zkHash as string
     }
   });
 
