3f69cd1b1dfdb26e9a52bed7e6e4fa326ed021e1
verumlotus <universumtremendum@gmail.com>
2022-06-04 15:05:51 -0400

    Scrape first names

---
+++ backendUtils/createRainbow.ts
@@ -0,0 +1,23 @@
+import axios from "axios";
+import fs from "fs";
+import path from "node:path"
+
+async function scrapeFirstNames() {
+    const URL = "https://www.usna.edu/Users/cs/roche/courses/s15si335/proj1/files.php%3Ff=names.txt&downloadcode=yes";
+    const response = await axios.get(URL);
+    // Now let's break by new line and store this all in a json file
+    let firstNames = []
+    firstNames = response.data.split(/\r?\n/)
+    // Delete our firstNames.json File if it exists
+    const FILE_PATH = path.resolve(__dirname, "firstNames.json");
+    if (fs.existsSync(FILE_PATH)) {
+        fs.unlinkSync(FILE_PATH)
+    }
+    // Write to our file now 
+    const dataToWrite = {
+        "firstNames": firstNames
+    }
+    fs.writeFileSync(FILE_PATH, JSON.stringify(dataToWrite))
+}
+
+scrapeFirstNames();
